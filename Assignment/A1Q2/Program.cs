2b43982dd114a67eceb5358ef50d477eee11b6f6
mb123-123 <makarandbhakare3103Gmail.com>
2024-12-27 18:58:21 +0530

    Assignment 1 and 2

---
+++ Assignment/A1Q2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Basic Calculator");
+            Console.WriteLine("");
+
+            Console.WriteLine("Enter the choice : \n1. Addition\n2.Subtraction\n3.Multiplication\n4.Division");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Enter the first number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            double result;
+            switch (operation)
+            {
+                case 0:
+                    Console.WriteLine("Exiting from program");
+                    Environment.Exit(0);
+                    break;
+
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("Result : " + result);
+                    break;
+
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("Result : " + result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("Result : " + result);
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    Console.WriteLine("Result : " + result);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Assignment/A1Q3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int operation;
+                double result;
+            do
+            {
+                Console.WriteLine("This Calculator is made using Function");
+                Console.WriteLine("");
+
+
+                Console.WriteLine("Enter the choice : \n0. Exit\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+                operation = Convert.ToInt32(Console.ReadLine());
+
+                if (operation == 0)
+                {
+                    Console.WriteLine("Exiting from program !!!!");
+                    break;  
+                }
+               
+                Console.WriteLine("Enter the num1 : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the num2 : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (operation)
+                {
+                    case 1:
+                        result = num1 + num2;
+                        Console.WriteLine("Result : " + result);
+                        break;
+                    case 2:
+                        result = num1 - num2;
+                        Console.WriteLine("Result : " + result);
+                        break;
+                    case 3:
+                        result = num1 *  num2;
+                        Console.WriteLine("Result : " + result);
+                        break;
+                    case 4:
+                        result = num1 / num2;
+                        Console.WriteLine("Result : " + result);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+            while (operation != 0);
+
+
+        }
+    }
+}
